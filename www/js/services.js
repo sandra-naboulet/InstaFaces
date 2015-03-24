3caeff29f88df1d42cb667498651c8df2d38963f
Antonin Riviere <antonin.rvr@gmail.com>
2015-03-24 01:30:54 +0100

    Add instagram search selfie

--- www/js/services.js
+++ www/js/services.js
@@ -30,15 +30,15 @@ angular.module('starter.services', [])
 
     getFaceInfos : function(urls, callback){
 
-        if(navigator && navigator.connection && navigator.connection.type === 'none'){
-          $ionicPopup.alert({
-            title:'Connexion impossible',
-            template:'Vous n\'etes pas connecté a Internet' 
-          });
-          return;
-        }
+      if(navigator && navigator.connection && navigator.connection.type === 'none'){
+        $ionicPopup.alert({
+          title:'Connexion impossible',
+          template:'Vous n\'etes pas connecté a Internet' 
+        });
+        return;
+      }
 
-        var url = "http://api.skybiometry.com/fc/faces/detect?callback=JSON_CALLBACK&output=jsonp&api_key=" + api_key + "&api_secret=" + api_secret + "&urls="+ urls +"&attributes=all" ;
+      var url = "http://api.skybiometry.com/fc/faces/detect?callback=JSON_CALLBACK&output=jsonp&api_key=" + api_key + "&api_secret=" + api_secret + "&urls="+ urls +"&attributes=all" ;
 
       $http.jsonp(url)
       .success(function(res){
@@ -52,5 +52,36 @@ angular.module('starter.services', [])
 
   };
 })
+
+
+.factory("InstaService", function($http,$ionicLoading,$ionicPopup){ 
+
+  var clientid = "2e5abd064927444aa13fc2c5d27ef800";
+  var tag = "selfie";
+  var count = 1;
+  return{
+
+    fetchSelfies: function(callback) {
+      var url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?client_id="+clientid+"&callback=JSON_CALLBACK";
+
+
+      if(navigator && navigator.connection && navigator.connection.type === 'none'){
+        $ionicPopup.alert({
+          title:'Connexion impossible',
+          template:'Vous n\'etes pas connecté a Internet' 
+        });
+        return;
+      }
+
+      $http.jsonp(url).success(function(response) {
+        $ionicLoading.hide();
+        callback(response.data);
+      });
+    }
+
+
+  };
+})
+
 
 ;
