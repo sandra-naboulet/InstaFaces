1f89512ebba0e7b3d6f57b73eba6f2ecd23d184b
Antonin Riviere <antonin.rvr@gmail.com>
2015-03-24 12:47:30 +0100

    instagram fectch and slide

--- www/js/controllers/InstaCtrl.js
+++ www/js/controllers/InstaCtrl.js
@@ -1,16 +1,30 @@
 
 var app= angular.module('starter');
 
-app.controller('InstaCtrl', function($scope, InstaService){
+app.controller('InstaCtrl', function($scope, InstaService, $interval, $ionicSlideBoxDelegate){
   $scope.imgcontain = 0;
+  $scope.result = [];
   $scope.yolo = "Search Instagram Selfies";
+
+  // Fix ionic slider with ng-repeat
+  setTimeout(function(){
+      $ionicSlideBoxDelegate.update();
+      console.log('setTimeout');
+  },1000);
+
+  $scope.init = function () {
+   $scope.getSelfies();
+  };
 
   $scope.getSelfies = function() {
     InstaService.fetchSelfies(function(response){
-      console.log('hello');
+      console.log('hello getSelfies');
       $scope.imgcontain = 1;
-      $scope.img_url = response[0].images.standard_resolution.url;
+      $scope.img_url2 = response;
+      $scope.result = response;
+
     });
   };
+  
 
 });
