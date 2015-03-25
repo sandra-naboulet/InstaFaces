c4339ce4d4d7a065b8bb2638073e92e667bc7d16
Sandra Naboulet <naboul_s@etna-alternance.net>
2015-03-25 11:43:25 +0100

    Front camera

--- www/js/controllers/InstaCtrl.js
+++ www/js/controllers/InstaCtrl.js
@@ -36,6 +36,7 @@ app.controller('InstaCtrl', function($scope, InstaService, FaceService, $interva
   var options =   {   quality: 50,
                       destinationType: Camera.DestinationType.DATA_URL, // FILE_URI or DATA_URL
                       sourceType: Camera.PictureSourceType.CAMERA, // 0:Photo Library, 1=Camera, 2=Saved Album
+                      cameraDirection: 1,
                       encodingType: 0     // 0=JPG 1=PNG
                   };
 
