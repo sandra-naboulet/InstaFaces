3bbc66b9d2f3462a0009b499fa1bf8b585f8ca7d
Sandra NABOULET <snaboulet@modulotech.fr>
2015-03-26 14:53:34 +0100

    try to fix error

--- www/js/controllers/GameCtrl.js
+++ www/js/controllers/GameCtrl.js
@@ -1,65 +1,5 @@
 var app= angular.module('starter');
 
-<<<<<<< HEAD
-app.controller('GameCtrl', function($scope, InstaService, FaceService, $interval, $ionicSlideBoxDelegate){
-
-	$scope.init = function () {
-		alert('THIS IS BEFORE CAMERA');
-		$scope.openCamera();
-	};
-/*
-  $scope.shareSelfie = function (){
-    var insta = document.getElementById('ImageCapture');
-    var caption = "Hey @antonin_rvr, you've been selfimitated!!! "
-    $cordovaInstagram.share(insta, caption).then(function() {
-      console.log('GameCtrl, share on onstagram worked');
-  }, function(err) {
-      console.log('No share on instagram');
-  });
-  };*/
-
-
-	$scope.openCamera = function() {
-
-		if (!navigator.camera) {
-			alert("Camera API not supported", "Error");
-			return;
-		}
-
-		var options =   
-		{   quality: 50,
-          destinationType: Camera.DestinationType.DATA_URL, // FILE_URI or DATA_URL
-          sourceType: Camera.PictureSourceType.CAMERA, // 0:Photo Library, 1=Camera, 2=Saved Album
-          cameraDirection: 1,
-          encodingType: 0     // 0=JPG 1=PNG
-      };
-
-      navigator.camera.getPicture(
-      	  //Succes
-      	  function(imgData) {
-        	  var theImage = document.getElementById('ImageCapture');
-        		theImage.style.display = 'block';
-        		theImage.src = "data:image/jpeg;base64," + imgData;
-            console.log(theImage.src);
-            // Put image in canvas
-            var canvas = document.getElementById('insta');
-            var ctx = canvas.getContext('2d');
-            var sendselfie = new Image();
-            sendselfie.src = theImage.src; 
-            ctx.drawImage(sendselfie, 0, 0);
-
-            // Get image URL
-            FaceService.getImgurUrl(imgData, function(res){
-
-            	var imgUrl = res.data.link;
-
-            	alert('In GameCtrl : url = ' + imgUrl);
-
-                // Get Face infos
-                FaceService.getFaceInfos(imgUrl, function(face){
-                	console.log('glass ? ' + face.glass);
-                });
-=======
 app.controller('GameCtrl', function($scope, $ionicLoading, InstaService, FaceService, $interval, $ionicSlideBoxDelegate, $stateParams){
 
 // $scope.tag = $routeParams.tag;
@@ -81,7 +21,6 @@ app.controller('GameCtrl', function($scope, $ionicLoading, InstaService, FaceSer
     $ionicLoading.show({template: 'Loading...'});
     $scope.getSelfies();
   };
->>>>>>> ba88bd1178bf173be87aa89a439ac27eee696b5f
 
   $scope.getSelfies = function() {
     
--- www/js/controllers/ResultCtrl.js
+++ www/js/controllers/ResultCtrl.js
@@ -16,50 +16,56 @@ app.controller('ResultCtrl', function($scope, InstaService, FaceService, $interv
   }, function(err) {
       console.log('No share on instagram');
   });
-  };*/
+};*/
 
-  $scope.getResult = function(face1, face2) {
-    return face1.compareWith(face2);
-  };
+$scope.getResult = function(face1, face2) {
+  return face1.compareWith(face2);
+};
 
-  $scope.share = function(url1, url2) {
-    
-  };
+$scope.share = function(url1, url2) {
+  
+};
 
-  $scope.next = function() {
-   
-  };
+$scope.next = function() {
+ 
+};
 
-  $scope.retry = function() {
-    
-  };
+$scope.retry = function() {
+  
+};
 
 
-  $scope.openCamera = function() {
 
-    if (!navigator.camera) {
-      alert("Camera API not supported", "Error");
-      return;
-    }
+$scope.openCamera = function() {
 
-    var options =   
-    {   quality: 50,
+  if (!navigator.camera) {
+    alert("Camera API not supported", "Error");
+    return;
+  }
+
+  var options =   
+  {   quality: 50,
           destinationType: Camera.DestinationType.DATA_URL, // FILE_URI or DATA_URL
           sourceType: Camera.PictureSourceType.CAMERA, // 0:Photo Library, 1=Camera, 2=Saved Album
           encodingType: Camera.EncodingType.JPEG,
           targetWidth: 720, 
           cameraDirection: 1,
           encodingType: 0     // 0=JPG 1=PNG
-      };
+        };
 
-      navigator.camera.getPicture(
+        navigator.camera.getPicture(
           //Succes
           function(imgData) {
             var theImage = document.getElementById('ImageCapture');
             theImage.style.display = 'block';
             theImage.src = "data:image/jpeg;base64," + imgData;
-          
-            console.log('w=' + theImage.width + ' h=' + theImage.height);
+            console.log(theImage.src);
+                // Put image in canvas
+                var canvas = document.getElementById('insta');
+                var ctx = canvas.getContext('2d');
+                var sendselfie = new Image();
+                sendselfie.src = theImage.src; 
+                ctx.drawImage(sendselfie, 0, 0);
 
             // Get image URL
             FaceService.getImgurUrl(imgData, function(res){
@@ -68,13 +74,13 @@ app.controller('ResultCtrl', function($scope, InstaService, FaceService, $interv
 
               alert('In GameCtrl : url = ' + imgUrl);
 
-                // Get Face infos
-                FaceService.getFaceInfos(imgUrl, function(face){
-                  console.log('glass ? ' + face.glass);
-                });
+                    // Get Face infos
+                    FaceService.getFaceInfos(imgUrl, function(face){
+                      console.log('glass ? ' + face.glass);
+                    });
 
-            });
-        },
+                  });
+          },
           //Error
           function() {
             console.log('In GameCtrl : error ');
@@ -82,6 +88,6 @@ app.controller('ResultCtrl', function($scope, InstaService, FaceService, $interv
         //Options
         options);
 
-      return false;
-  };
+return false;
+};
 });
