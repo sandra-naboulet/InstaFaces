7653434cb22cd148313e6eb78962251db3c05673
Antonin Riviere <antonin.rvr@gmail.com>
2015-03-26 21:31:33 +0100

    comparaison scoring

--- www/js/services/FaceService.js
+++ www/js/services/FaceService.js
@@ -12,10 +12,127 @@ var Face = function () {
     this.pitch;
     this.yaw;
     this.roll;
+    this.width;
   }
  
   Face.prototype.compareWith = function (anotherFace) {
-    return 42;
+    var smile1 = this.smiling;
+    var glass1 = this.glass;
+    var pitch1 = this.pitch;
+    var yaw1 = this.yaw;
+    var roll1 = this.roll;
+    var width1 = this.width;
+
+    var smile2 = anotherFace.smiling;
+    var glass2 = anotherFace.glass;
+    var pitch2 = anotherFace.pitch;
+    var yaw2 = anotherFace.yaw;
+    var roll2 = anotherFace.roll;
+    var width2 = anotherFace.width;
+
+    var score_smile;
+    var score_glass;
+    var score_pitch;
+    var score_yaw;
+    var score_roll;
+    var score_width;
+
+    var final_score;
+
+    // Echelle de smile 
+    var result_smile = smile1 - smile2;
+    result_smile = Math.abs(result_smile);
+    if(result_smile <10){
+        score_smile = 100;
+    }
+    else if(result_smile <20){
+        score_smile = 90;
+    }
+    else if(result_smile <30){
+        score_smile = 70;
+    }
+    else if(result_smile <40){
+        score_smile = 50;
+    }
+    else if(result_smile <50){
+        score_smile = 30;
+    }
+    else {
+        score_smile = 0;
+    }
+    // Echelle de roll
+    var result_roll = roll1 - roll2;
+    result_roll = Math.abs(result_roll);
+    if(result_roll <5){
+        score_roll = 100;
+    }
+    else if(result_roll <10){
+        score_roll = 80;
+    }
+    else if(result_roll <20){
+        score_roll = 60;
+    }
+    else if(result_roll <30){
+        score_roll = 30;
+    }
+    else {
+        score_roll = 0;
+    }
+    // Echelle de yaw
+    var result_yaw = yaw1 - yaw2;
+    result_yaw = Math.abs(result_yaw);
+    if(result_yaw <5){
+        score_yaw = 100;
+    }
+    else if(result_yaw <10){
+        score_yaw = 80;
+    }
+    else if(result_yaw <15){
+        score_yaw = 60;
+    }
+    else if(result_yaw <20){
+        score_yaw = 40;
+    }
+    else if(result_yaw <25){
+        score_yaw = 30;
+    }
+    else {
+        score_yaw = 0;
+    }
+
+    // Echelle de width
+    var result_width = width1 - width2;
+    result_width = Math.abs(result_width);
+    if(result_width <5){
+        score_width = 100;
+    }
+    else if(result_width <10){
+        score_width = 80;
+    }
+    else if(result_width <15){
+        score_width = 60;
+    }
+    else if(result_width <20){
+        score_width = 40;
+    }
+    else if(result_width <25){
+        score_width = 30;
+    }
+    else {
+        score_width = 0;
+    }
+
+    final_score = (score_width + score_yaw + score_roll + score_smile)/4;
+    //bonus glasses
+    if ((glass1 === glass2) && (final_score<95)){
+      final_score += 5;
+    }
+    if ((glass1 === glass2) && (final_score>95)){
+      final_score = 100;
+    }
+
+    return final_score;
+
   };
 
   Face.prototype.toString = function faceToString() {
