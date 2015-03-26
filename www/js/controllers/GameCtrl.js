1c98e320657cfa49082a897043b0cd2a7822a8f5
Antonin Riviere <antonin.rvr@gmail.com>
2015-03-26 02:26:31 +0100

    picture displayed and used on page results + some fixes

---
+++ www/js/controllers/GameCtrl.js
@@ -0,0 +1,57 @@
+var app= angular.module('starter');
+
+app.controller('GameCtrl', function($scope, InstaService, FaceService, $interval, $ionicSlideBoxDelegate){
+
+	$scope.init = function () {
+		alert('THIS IS BEFORE CAMERA');
+		$scope.openCamera();
+	};
+
+
+	$scope.openCamera = function() {
+
+		if (!navigator.camera) {
+			alert("Camera API not supported", "Error");
+			return;
+		}
+
+		var options =   
+		{   quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL, // FILE_URI or DATA_URL
+          sourceType: Camera.PictureSourceType.CAMERA, // 0:Photo Library, 1=Camera, 2=Saved Album
+          cameraDirection: 1,
+          encodingType: 0     // 0=JPG 1=PNG
+      };
+
+      navigator.camera.getPicture(
+      	  //Succes
+      	  function(imgData) {
+        	  var theImage = document.getElementById('ImageCapture');
+        		theImage.style.display = 'block';
+        		theImage.src = "data:image/jpeg;base64," + imgData;
+            console.log(theImage.src);
+
+            // Get image URL
+            FaceService.getImgurUrl(imgData, function(res){
+
+            	var imgUrl = res.data.link;
+
+            	alert('In GameCtrl : url = ' + imgUrl);
+
+                // Get Face infos
+                FaceService.getFaceInfos(imgUrl, function(face){
+                	console.log('glass ? ' + face.glass);
+                });
+
+            });
+        },
+          //Error
+          function() {
+          	console.log('In GameCtrl : error ');
+          },
+        //Options
+        options);
+
+      return false;
+  };
+});
